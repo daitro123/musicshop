1c7838f4567ebec8fedf34c8cbb65f78b793e5a0
daitro123 <macek.tom@gmail.com>
2021-10-12 14:59:16 +0200

    First commit

---
+++ wp-content/themes/musicshop-timber/src/Lib/Setup/Core.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Theme\Lib\Setup;
+
+class Core
+{
+    public function __construct()
+    {
+        add_action(
+            'wp_enqueue_scripts',
+            [
+                $this,
+                'loadStyles'
+            ]
+        );
+
+        add_action(
+            'wp_enqueue_scripts',
+            [
+                $this,
+                'loadScripts'
+            ]
+        );
+
+        add_action(
+            'after_setup_theme',
+            [
+                $this,
+                'themeSupports'
+            ]
+        );
+    }
+
+    public function loadStyles()
+    {
+        wp_enqueue_style('theme-styles', get_stylesheet_directory_uri() . '/dist/style.min.css', [], filemtime(get_stylesheet_directory() . '/dist/style.min.css'));
+    }
+
+    public function loadScripts()
+    {
+        wp_enqueue_script('theme-scripts', get_stylesheet_directory_uri() . '/assets/main.min.js', [], filemtime(get_stylesheet_directory() . '/assets/main.min.js'), TRUE);
+    }
+
+    public function themeSupports()
+    {
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
+
+        /*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+        add_theme_support('title-tag');
+
+        /*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+        add_theme_support('post-thumbnails');
+
+        /*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+        add_theme_support(
+            'html5',
+            array(
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+            )
+        );
+
+        /*
+		 * Enable support for Post Formats.
+		 *
+		 * See: https://codex.wordpress.org/Post_Formats
+		 */
+        add_theme_support(
+            'post-formats',
+            array(
+                'aside',
+                'image',
+                'video',
+                'quote',
+                'link',
+                'gallery',
+                'audio',
+            )
+        );
+
+        add_theme_support('menus');
+
+        add_theme_support('woocommerce');
+
+        add_theme_support('wc-product-gallery-zoom');
+        add_theme_support('wc-product-gallery-lightbox');
+        add_theme_support('wc-product-gallery-slider');
+    }
+}
---
+++ wp-content/themes/musicshop-timber/webpack.config.js
@@ -0,0 +1,81 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+	entry: {
+		main: "./assets/js/main.js",
+		vendor: "./assets/js/vendor.js",
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].min.js",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						// translates CSS into CommonJS modules
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						// Run postcss actions
+						loader: "postcss-loader",
+						options: {
+							sourceMap: true,
+							postcssOptions: {
+								plugins: [
+									[
+										"autoprefixer",
+										{
+											grid: "autoplace",
+										},
+									],
+								],
+							},
+						},
+					},
+					{
+						// compiles Sass to CSS
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|webp)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "images/[name][ext]",
+				},
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "style.min.css",
+		}),
+		// new ImageMinimizerPlugin({
+		//     minimizerOptions: {
+		//         // Lossless optimization with custom option
+		//         // Feel free to experiment with options for better result for you
+		//         plugins: [
+		//             ["gifsicle", { interlaced: true }],
+		//             ["jpegtran", { progressive: true }],
+		//             ["optipng", { optimizationLevel: 5 }],
+		//         ],
+		//     },
+		// }),
+	],
+};
