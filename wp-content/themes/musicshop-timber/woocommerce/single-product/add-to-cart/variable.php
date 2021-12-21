3fabff63e846136d138fc91dbd48257611a81e98
daitro123 <macek.tom@gmail.com>
2021-12-21 09:41:31 +0100

    cart updates

--- wp-content/themes/musicshop-timber/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/musicshop-timber/woocommerce/single-product/add-to-cart/variable.php
@@ -67,6 +67,9 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
                                 );
                                 // echo end($attribute_keys) === $attribute_name ? wp_kses_post(apply_filters('woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__('Clear', 'woocommerce') . '</a>')) : '';
                                 ?>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+                                </svg>
                             </span>
                         </div>
                     <?php endforeach; ?>
